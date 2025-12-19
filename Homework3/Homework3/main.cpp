0fb972587d4a941b25b18b79a2f92d7c895417a0
challenge04su <challenge04su@gmail.com>
2025-12-19 20:05:28 +0900

    complete must homework
    [필수] 부분 다시 구현 완료!

--- Homework3/Homework3/main.cpp
+++ Homework3/Homework3/main.cpp
@@ -1,39 +1,105 @@
 #include <iostream>
 #include <string>
-#include "Item.h"
-#include "Inventory.h"
 
 using namespace std;
 
-int main()
+class Item
 {
-	int count;
+public:
+	Item()
+	{
+		name_ = "";
+		price_ = 0;
+	}
+	Item(string name, int price)
+	{
+		name_ = name;
+		price_ = price;
+	}
+	~Item() {}
+
+	string GetName() { return name_; }
+	int GetPrice() { return price_; }
+
+private:
 	string name_;
 	int price_;
+};
 
-	Inventory<Item> inventory(10);
+template <typename T>
+class Inventory
+{
+private:
+	T* pItems_;
+	int capacity_;
+	int size_;
 
-	cout << "추가할 아이템 개수를 입력하세요. : ";
-	cin >> count;
-	cin.ignore();
-	cout << endl;
-
-	for (int i = 0; i < count; i++)
+public:
+	Inventory(int capacity_ = 10)
 	{
-		cout << "아이템 이름 : ";
-		getline(cin, name_);
-		cout << endl;
-
-		cout << "아이템 가격 : ";
-		cin >> price_;
-		cin.ignore();
-		cout << endl;
-
-		Item item(name_, price_);
-		inventory.AddItem(item);
+		pItems_ = new Item[capacity_];
+		this->capacity_ = capacity_;
+		size_ = 0;
 	}
-	cout << endl;
-	inventory.PrintAllItems();
+	~Inventory()
+	{
+		delete[] pItems_;
+	}
 
+	void AddItem(Item item)
+	{
+		if (size_ >= capacity_)
+		{
+			cout << "인벤토리가 꽉 찼습니다!" << endl;
+			return;
+		}
+		pItems_[size_++] = item;
+
+	}
+	void RemoveLastItem()
+	{
+		if (size_ < 1)
+		{
+			cout << "인벤토리가 비어있습니다." << endl;
+			return;
+		}
+		else
+		{
+			size_--;
+		}
+	}
+	void PrintAllItems()
+	{
+		if (size_ < 1)
+		{
+			cout << "인벤토리가 비어있습니다." << endl;
+			return;
+		}
+		else
+		{
+			for (int i = 0; i <= size_ - 1; i++)
+			{
+				cout << "Name : " << pItems_[i].GetName() << ", Price : " << pItems_[i].GetPrice() << endl;
+			}
+		}
+	}
+};
+
+
+
+int main()
+{
+	Item item("Hello Item", 1000);
+	Inventory<Item> inventory;
+	inventory.AddItem(item);
+	inventory.AddItem(Item("Hello Item2", 1100));
+
+	for (int i = 0; i < 10; i++)
+	{
+		inventory.AddItem(Item("Hello Item" + to_string(i), 100));
+	}
+
+	inventory.RemoveLastItem();
+	inventory.PrintAllItems();
 	return 0;
 }
